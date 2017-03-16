28b91da37f897e111b1a512083acf72f69f0daab
anuragkrishna <anurag.bits18@gmail.com>
2017-03-16 15:26:33 +0530

    replaced form with div

--- src/component/ChatForm.js
+++ src/component/ChatForm.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <form id="chat_form" action="">
+        <div id="chat_form" action={props.sendMessage}>
     		<div className="input-group">
       			<input type="text" autoComplete="off" className="form-control" value={props.message} onChange={props.onChange} placeholder="Write something"/>
       			<span className="btn btn-primary input-group-addon" onClick={props.sendMessage}>Send</span>
     		</div>
-  		</form>
+  		</div>
     );
 }
