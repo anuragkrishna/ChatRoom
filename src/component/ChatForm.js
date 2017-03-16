b196357348729cd5a089685ee3a884bf29723ab5
anuragkrishna <anurag.bits18@gmail.com>
2017-03-16 15:43:33 +0530

    form-horizontal: without thise firefox was misbehaving

--- src/component/ChatForm.js
+++ src/component/ChatForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <div id="chat_form" action={props.sendMessage}>
+        <div id="chat_form form-horizontal" action={props.sendMessage}>
     		<div className="input-group">
       			<input type="text" autoComplete="off" className="form-control" value={props.message} onChange={props.onChange} placeholder="Write something"/>
       			<span className="btn btn-primary input-group-addon" onClick={props.sendMessage}>Send</span>
